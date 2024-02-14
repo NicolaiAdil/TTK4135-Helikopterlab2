864fbf196087e17bf59119040b54b248c152de8f
fredrabj <fredrabj@gmail.com>
2024-02-14 13:15:55 +0100

    added init_simulator plot

--- dag2/preparations/init_simulator.m
+++ dag2/preparations/init_simulator.m
@@ -162,6 +162,12 @@ x2  = [zero_padding; x2; zero_padding];
 x3  = [zero_padding; x3; zero_padding];
 x4  = [zero_padding; x4; zero_padding];
 
+t = 0:delta_t:delta_t*(length(u)-1);
+plot(t,x1*180/pi), grid
+ylabel("travel")
+
+
+
 u_simulink = timeseries(u,t);
 
 
