d066b9f605b4b4b007ee70cf42b84f33ac62f2a5
fredrabj <fredrabj@gmail.com>
2024-04-10 18:38:01 +0200

    feat: fixed lab preparation

--- dag4/preparations/constraints.m
+++ dag4/preparations/constraints.m
@@ -1,5 +1,9 @@
 function [c,ceq] = constraints(z)
-    global alpha beta lambda_t nx N
+    alpha = 0.2;
+    beta = 20;
+    lambda_t = 2*pi/3;
+    nx = 6;
+    N = 40;
     c = zeros(N,1);
     for k=1:N
         c(k) = alpha*exp(-beta*(z(1+(k-1)*nx)-lambda_t)^2) - z(5+(k-1)*nx);
